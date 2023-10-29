e3a330983bb67339ecb30a9088708f0d387b1a6a
Esther García <esther.garcia@intelligenia.com>
2023-10-29 13:44:16 +0100

    Update functions
    Separated from views

--- e-commerce/ecommerce/functions.py
+++ e-commerce/ecommerce/functions.py
@@ -7,36 +7,51 @@ from django.http import HttpResponse
 from etienda import models as ecommerce_models
 from django.conf import settings
 
-# Database connection
-# https://pymongo.readthedocs.io/en/stable/tutorial.html
-client = MongoClient('mongo', 27017)
-store_db = client.store  # Database
-product_collection = store_db.products  # Collection
-api = 'https://fakestoreapi.com/products'  # API to get products
+
+def get_product_collection():
+    """Get the product collection"""
+    # Database connection
+    # https://pymongo.readthedocs.io/en/stable/tutorial.html
+    client = MongoClient('mongo', 27017)
+    store_db = client.store  # Database
+    return store_db.products  # Collection
 
 
 def get_image(url, id):
     """Get image from url and store it in the static folder"""
     data = requests.get(url).content
-    path = str(settings.STATIC_ROOT) + 'img_' + str(id) + '.jpg'
+    path = str(settings.STATIC_ROOT) + '/img_' + str(id) + '.jpg'
     file = open(path, 'wb')
     file.write(data)
     file.close()
     return path
 
 
+def import_products(request):
+    """Import products from the api if the database is empty"""
+    product_collection = get_product_collection()
+    if product_collection.count_documents({}) == 0:
+        products = get_products()
+        store_products(products)
+        return HttpResponse("Products imported")
+    else:
+        return HttpResponse("Database is already populated")
+
+
 def store_products(products):
     """Stores the products in the database"""
+    print("PRODS", products)
     for prod in products:
         try:
             product = ecommerce_models.Product(**prod)  # Get the product JSON
             product.image = get_image(prod.get('image'),
                                       prod.get('id'))  # Override Url validator changing url type
-            product_collection.insert_one(product.model_dump())  # Insert the product
+            get_product_collection().insert_one(product.model_dump())  # Insert the product
         except Exception as err:
             print('Something went wrong while storing the product -->', str(err))
             break
-    return HttpResponse("Imported " + str(product_collection.count_documents({})) + " products")
+    return HttpResponse("Imported " + str(get_product_collection().count_documents({}))
+                        + " products")
 
 
 def fill_database(request):
@@ -53,52 +68,58 @@ def fill_database(request):
 def truncate_database(request):
     """Delete database data"""
     try:
+        client = MongoClient('mongo', 27017)
         client.store.products.drop()
     except Exception as err:
         print('Something went wrong while truncating the database -->', str(err))
 
     return HttpResponse("Database truncated")
 
+
 def get_products():
     """Get products from the api
 
     https://requests.readthedocs.io/en/latest/
     """
+    api = 'https://fakestoreapi.com/products'  # API to get products
+
     try:
         response = requests.get(api).json()
     except Exception as err:
-        print("The API is down. Using decoy database")
-        response = [{"id": 1, "title": "fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-                     "price": 109.95,
-                     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-                     "category": "men's clothing",
-                     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-                     "rating": {"rate": 3.9, "count": 120}},
-                    {"id": 2, "title": "Mens Casual Premium Slim Fit T-Shirts ", "price": 22.3,
-                     "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-                     "category": "men's clothing",
-                     "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-                     "rating": {"rate": 4.1, "count": 259}},
-                    {"id": 3, "title": "Mens Cotton Jacket", "price": 55.99,
-                     "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-                     "category": "men's clothing",
-                     "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-                     "rating": {"rate": 4.7, "count": 500}},
-                    {"id": 4, "title": "Mens Casual Slim Fit", "price": 15.99,
-                     "description": "The color could be slightly different between on the screen "
-                                    "and in practice. / Please note that body builds vary by "
-                                    "person, therefore, detailed size information should be "
-                                    "reviewed below on the product description.",
-                     "category": "men's clothing",
-                     "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-                     "rating": {"rate": 2.1, "count": 430}},
-                    {"id": 5,
-                     "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-                     "price": 695,
-                     "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-                     "category": "jewelery",
-                     "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-                     "rating": {"rate": 4.6,
-                                "count": 400}},
-                    ]
+        print("The API is down. Try again later")
+        response = None
     return response
+
+
+def response_to_json(products):
+    """Converts the response to json"""
+    response = []
+    for prod in products:
+        response.append(prod)
+    return response
+
+
+def print_total_price_by_category(bill):
+    for category in bill:
+        print("Category:", category.get('_id'), "\tTotal:", category.get('count'), "€")
+
+
+def print_products(products, small):
+    """Pretty print products"""
+    print("Found", len(list(products.clone())), "!")  # Cloning cursor to avoid consuming it
+
+    if small:
+        for prod in products:
+            print("PRODUCT: (" + str(prod.get('_id')) + ")",
+                  prod.get('title'),
+                  "\t[" + str(prod.get('price')) + "€]",
+                  "\tCategory:", prod.get('category'),
+                  "\tRating:", prod.get('rating').get('rate'))
+    else:
+        for prod in products:
+            print("--------------------------------------")
+            print("PRODUCT: (" + str(prod.get('_id')) + ")",
+                  prod.get('title'),
+                  "\t[" + str(prod.get('price')) + "€]",
+                  "\nDESCRIPTION:", prod.get('description'))
+
--- e-commerce/etienda/views.py
+++ e-commerce/etienda/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from ecommerce.functions import product_collection, get_products, store_products
+from ecommerce import functions as ecommerce_functions
 
 
 def index(request):
@@ -10,7 +10,7 @@ def index(request):
     return render(request, 'landing.html', context=None)
 
 
-def get_products(category):
+def get_products_from(category):
     """Get products from the selected category"""
     product_collection = filter_product(None, None, None, None, None, category, None, None)
     products = []
@@ -34,19 +34,19 @@ def categories(request):  # TODO: migración inicial de datos
 
 def mens_clothing(request):
     """Get all men's clothing"""
-    import_products(request)
+    ecommerce_functions.import_products(request)
 
     # Filter products by category 'men's clothing'
-    products = get_products('men\'s clothing')
+    products = get_products_from('men\'s clothing')
     return render(request, 'products.html', context={'products': products})
 
 
 def womens_clothing(request):
     """Get all women's clothing"""
-    import_products(request)
+    ecommerce_functions.import_products(request)
 
     # Filter products by category 'men's clothing'
-    products = get_products('women\'s clothing')
+    products = get_products_from('women\'s clothing')
     return render(request, 'products.html', context={'products': products})
 
 
@@ -105,7 +105,7 @@ def filter_product(_id, title, min_price, max_price, description, category, scor
     if category is not None:  # Case-insensitive
         query.append({'category': {'$regex': f'^{category}', "$options": 'i'}})
 
-    result = product_collection.find({'$and': query})  # Operator 'and' needed in case there is a
+    result = ecommerce_functions.get_product_collection().find({'$and': query})  # Operator 'and' needed in case there is a
     # query over the same field (p.e: price)
 
     if order is not None:  # Ordering the result
@@ -118,7 +118,7 @@ def aggregate_by_product():
     pipeline = [
         {"$group": {"_id": "_id", "count": {"$sum": '$price'}}},
     ]
-    return product_collection.aggregate(pipeline)
+    return ecommerce_functions.get_product_collection().aggregate(pipeline)
 
 
 def aggregate_by_category():
@@ -126,101 +126,9 @@ def aggregate_by_category():
     pipeline = [
         {"$group": {"_id": "$category", "count": {"$sum": '$price'}}},
     ]
-    return product_collection.aggregate(pipeline)
+    return ecommerce_functions.get_product_collection().aggregate(pipeline)
 
 
-def print_products(products, small):
-    """Pretty print products"""
-    print("Found", len(list(products.clone())), "!")  # Cloning cursor to avoid consuming it
-
-    if small:
-        for prod in products:
-            print("PRODUCT: (" + str(prod.get('_id')) + ")",
-                  prod.get('title'),
-                  "\t[" + str(prod.get('price')) + "€]",
-                  "\tCategory:", prod.get('category'),
-                  "\tRating:", prod.get('rating').get('rate'))
-    else:
-        for prod in products:
-            print("--------------------------------------")
-            print("PRODUCT: (" + str(prod.get('_id')) + ")",
-                  prod.get('title'),
-                  "\t[" + str(prod.get('price')) + "€]",
-                  "\nDESCRIPTION:", prod.get('description'))
-            # "\nCATEGORY:", prod.get('category'),
-            # "\nIMAGE:", prod.get('image'),
-            # "\nRATING :", prod.get('rating').get('rate'))
 
 
-def response_to_json(products):
-    """Converts the response to json"""
-    response = []
-    for prod in products:
-        response.append(prod)
-    return response
 
-
-def print_total_price_by_category(bill):
-    for category in bill:
-        print("Category:", category.get('_id'), "\tTotal:", category.get('count'), "€")
-
-
-def import_products(request):
-    """Import products from the api if the database is empty"""
-    if product_collection.count_documents({}) == 0:
-        products = get_products()
-        store_products(products)
-        return HttpResponse("Products imported")
-    else:
-        return HttpResponse("Database is already populated")
-
-
-# EJERCICIO 1
-def ejercicio1(request):
-    import_products(request)
-    print("Finding electronic products between 100 and 200 euros, sorted by price...")
-    products = filter_product(None, None, 100, 200, None, "electronics", None, "price")
-    response = response_to_json(products)
-    return HttpResponse(response)
-
-
-# EJERCICIO 2
-def ejercicio2(request):
-    import_products(request)
-    print("Finding products containing the word 'pocket' in the description...")
-    products = filter_product(None, None, None, None, 'Pocket', None, None, None)
-    response = response_to_json(products)
-    return HttpResponse(response)
-
-
-# EJERCICIO 3
-def ejercicio3(request):
-    import_products(request)
-    print("Finding products with a score greater than 4...")
-    products = filter_product(None, None, None, None, None, None, 4, None)
-    response = response_to_json(products)
-    return HttpResponse(response)
-
-
-# EJERCICIO 4
-def ejercicio4(request):
-    import_products(request)
-    print("Finding men's clothing, sorted by rating...")
-    products = filter_product(None, None, None, None, None, "men's clothing", None, 'rating.rate')
-    response = response_to_json(products)
-    return HttpResponse(response)
-
-
-# EJERCICIO 5
-def ejercicio5(request):
-    import_products(request)
-    total = list(aggregate_by_product())
-    return HttpResponse("Total billed: " + str(total[0].get('count')) + "€")
-
-
-# EJERCICIO 6
-def ejercicio6(request):
-    import_products(request)
-    bill = aggregate_by_category()
-    bill = response_to_json(bill)
-    return HttpResponse("Total billed by category: " + str(bill))
